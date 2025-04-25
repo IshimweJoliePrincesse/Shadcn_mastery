d1cee1df5637c46dcbe1a15d00644ca2fd824c1c
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2025-04-25 19:28:46 +0200

    new changes

---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const { toggleSidebar } = useSidebar();
+
+  return (
+    <nav className="p-4 flex items-center justify-between sticky top-0 bg-background z-10">
+      <SidebarTrigger />
+
+      <div className="flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
